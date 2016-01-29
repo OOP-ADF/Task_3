127a1db6dd81cedeae5a852f6c4c8b0f24036cab
agungrbudiman <agungrbudiman@gmail.com>
2016-01-29 08:51:45 +0700

    finish
    getAppList @AppStore.class belum fix

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,25 @@
+//Agung Rahmat Budiman
+//1301144142
+//IF-38-02
 
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int totalApp;
+	
+	public void createNewApp(String appName, int appSize) {
+		totalApp++;
+		appList[totalApp] = new Application(appName, appSize);
+	}
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	public int getTotalApp() {
+		return totalApp;
+	}
+	public Application getAppList() {
+		return appList[100];
+	}
+	public String toString() {
+		return "There are "+totalApp+" applications ready to install";
+	}
+}
--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,22 @@
+//Agung Rahmat Budiman
+//1301144142
+//IF-38-02
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application (String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	public String getappName(){
+		return appName;
+	}
+	public int getappSize(){
+		return appSize;
+	}
+	public String toString() {
+		return "Application name: "+appName+", with size: "+appSize+"MB";
+	}
+}
