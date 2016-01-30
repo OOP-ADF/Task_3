cde122acfc884bc79d0fb1a2bc92571fcea3f867
Lemniscate <charmy.rse@gmail.com>
2016-01-31 01:18:10 +0700

    As you wish my master
    Nama : Ubassy Abdillah
    NIM : 1301148282
    Kelas : IF-38-02

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,30 @@
+//Nama : Ubassy Abdillah
+//NIM : 1301148282
 
+public class AppStore {
+	private Application app;
+	private Application[] appList= new Application[100];
+	private int totalApp=-1;
+	
+	public void createNewApp(String appName, int appSize) {
+		totalApp++;
+		app=new Application(appName,appSize);
+		appList[totalApp]=app;
+	}
+
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp+1;
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return "There are "+(totalApp+1)+" applications ready to install";
+	}
+}
--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,37 @@
+//Nama : Ubassy Abdillah
+//NIM : 1301148282
 
+public class SmartPhone {
+	private Application[] appList= new Application[10];
+	private int totalMemory,memory,totalApp=-1;
+	
+	public void setMemory(int memory) {
+		this.memory=memory;
+		totalMemory=memory;
+	}
+	
+	public void addApplication(AppStore appStore, int appId) {
+		Application app = appStore.getAppList()[appId];
+		if (app.getAppSize()<=memory){
+			totalApp++;
+			appList[totalApp]=app;
+			memory=memory-app.getAppSize();
+		}
+	}
+
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp+1;
+	}
+	
+	public int getRemainingSize() {
+		return memory;
+	}
+	
+	public String toString() {
+		return "Mermory size "+totalMemory+"MB, "+(totalApp+1)+" applications installed, remaining memory size: "+memory+"MB";
+	}
+}
