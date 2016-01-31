5f10280421fc5b391cca71c614b7c6050bf62480
Faiz Rahiemy <frachiemy@gmail.com>
2016-01-31 21:38:37 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,33 @@
 
+//Name 		 : Mohammad Zakie Faiz Rahiemy
+//Student ID : 1301144422
+//Class 	 : IF-38-02
+
+public class AppStore{
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	public void createNewApp(String appName,int appSize){
+		Application app;
+		app = new Application(appName,appSize);
+		appList[totalApp] = app;
+		totalApp++;
+	}
+	
+	public Application getApp(int id){
+		return appList[id];
+	}
+	
+	public int getTotalApp(){
+		return totalApp;
+	}
+	
+	public Application[] getAppList(){
+		return appList;
+	}
+	
+	public String toString(){
+		return "There are "+totalApp+" applications ready to install";
+	}
+}
+
