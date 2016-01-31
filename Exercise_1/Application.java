1f83e4af9b2671a9187e3c5b743cab8ee2e6536a
Faiz Rahiemy <frachiemy@gmail.com>
2016-01-31 21:30:38 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,25 @@
+//Name 		 : Mohammad Zakie Faiz Rahiemy
+//Student ID : 1301144422
+//Class 	 : IF-38-02
 
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName,int appSize){
+		this.appName=appName;
+		this.appSize=appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	
+	public int getAppSize(){
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name: "+appName+", with size: "+appSize+"MB");
+	}
+}
