e552bc3989c87b1ebfaf894b7a6ee3399944d214
daintyamanda <daintyamandaf@gmail.com>
2016-01-31 19:01:45 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,35 @@
+/*Nama : Dainty Amanda Fathurrohmi
+Nim : 1301140062
+Kelas : IF 38-02*/
 
+public class SmartPhone {
+	private Application[] smAppList = new Application[100];
+	private int totalApp;
+	private int memory;
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	
+	public int getRemainingSize() {
+		int m = memory;
+		int i, used;
+		i=0;
+		used=0;
+		while (smAppList[i] != null) {
+			used = used + smAppList[i].getAppSize();
+			i++;
+		}
+		return m=m-used;
+	}
+	
+	public void addApplication(AppStore appStore, int appId) {
+		if (getRemainingSize() >= appStore.getApp(appId).getAppSize()) {
+			smAppList[totalApp]=appStore.getApp(appId);
+			totalApp++;
+		}
+	}
+	
+ 	public String toString() {
+		return ("memory size "+memory+"MB, "+totalApp+" application installed, remaining memory size : "+getRemainingSize()+"MB");
+	}
+}
