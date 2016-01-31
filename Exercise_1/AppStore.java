0fcf7aeb31398f6950926abdc19b8cdf84bbdfab
daintyamanda <daintyamandaf@gmail.com>
2016-01-31 18:57:37 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,24 @@
+/*Nama : Dainty Amanda Fathurrohmi
+Nim : 1301140062
+Kelas : IF 38-02*/
 
+public class AppStore {
+	private Application[] appList = new Application[100];
+ 	private int totalApp=0;
+ 	
+ 	public void createNewApp(String appName, int appSize) {
+ 		Application app = new Application(appName, appSize);
+ 		appList[totalApp] = app;
+ 		totalApp++;
+ 	}
+ 	
+ 	public Application getApp(int id) {
+		return appList[id];
+ 	}
+ 	
+	public String toString() {
+ 		return ("There are "+totalApp+" applications ready to install");
+	}
+	
+	
+}
