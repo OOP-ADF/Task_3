2fafb538bc2d64e6305dd8c5c04969e4fef860f5
bimamboell <fitrahbiman@yahoo.co.id>
2016-02-02 01:20:48 +0700

    a
    a

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,26 @@
+//Fitrah Bima Nusantara
+//1301140295
+//IF 38-09
+ 
+public class Application {
+	private String namaApp;
+	private int sizeApp;
+	
+	public Application(String namaApp, int sizeApp) {
+		this.namaApp = namaApp;
+		this.sizeApp = sizeApp;
+	}
+	
+	public String getnamaApp() {
+		return namaApp;
+	}
+	
+	public int getsizeApp() {
+		return sizeApp;
+	}
+	
+	public String toString() {
+		return ("Application name : "+namaApp+", with size : "+sizeApp+"MB");
+	}
+}
 
