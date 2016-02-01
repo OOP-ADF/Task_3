d4199f0dce6c2a15a7a0b8ccf2df087c35ab940f
FerdyDianAnugrah <ferdykroco01@yahoo.com>
2016-02-02 06:44:48 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,28 @@
+// Ferdy Dian Anugrah(1301140035) IF 3809
+ 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public void setappName(String appName) {
+		this.appName = appName;
+	}
+	
+	public String toString() {
+		return ("Application name : "+appName+", with size : "+appSize+"MB");
+	}
+}
 
