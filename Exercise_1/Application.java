aee4021bd8f5f80813ea267d5d96d47ed6fa8f1a
rizkymuhrifai <rizdec95@gmail.com>
2016-02-01 20:26:06 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,25 @@
-
+/* Nama 	: Rizky Muhammad Rifai
+	 NIM  	: 1301144099
+	 Kelas	: IF-38-09 */
+	 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+ 	public String getAppName() {
+		return appName;
+ 	}
+ 	
+ 	public int getAppSize() {
+ 		return appSize;
+ 	}
+ 	
+ 	public String toString() {
+ 		return ("Application name : "+appName+", with size : "+appSize);
+ 	}
+}
