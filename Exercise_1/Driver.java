0508d64fc390929566e86e5c54b0b38555175a71
adzannugraha <adzan.nugraha@gmail.com>
2016-02-01 23:40:59 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,21 @@
+//IF-38-09_Adzan Nugraha_1301140249
 
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		AppStore aps = new AppStore();
+		aps.createNewApp("app1",100);
+		aps.createNewApp("app2",200);
+		aps.createNewApp("app3",300);
+		aps.createNewApp("app4",400);
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(2).toString());
+		
+		SmartPhone sm = new SmartPhone();
+		sm.setMemory(300);
+		System.out.println(sm.toString());
+		sm.addApplication(aps, 3);
+		System.out.println(sm.toString());
+	}
+}
