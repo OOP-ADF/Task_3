a697d8df485613fbcf3348675d716e091fc6bada
FerdyDianAnugrah <ferdykroco01@yahoo.com>
2016-02-02 06:44:13 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,30 @@
+// Ferdy Dian Anugrah(1301140035) IF 3809
+
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	public void createNewApp(String appName, int appSize) {
+		Application newApp = new Application(appName, appSize);
+		appList[totalApp] = newApp;
+		totalApp++;
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return "There are "+totalApp+" application ready to install";
+	}
+}
+
 
