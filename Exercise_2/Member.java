4ae4c228ccfcfe1eb69ffc943afb2b229892fd3f
rizkymuhrifai <rizdec95@gmail.com>
2016-02-01 20:42:15 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,44 +1,44 @@
 /*
-	Nama 	: Rizky Muhammad Rifai
-	NIM  	: 1301144099
-	Kelas	: IF-38-09
+Nama 	: Rizky Muhammad Rifai
+NIM  	: 1301144099
+Kelas	: IF-38-09
 */
 
 public class Member {
-	private String name;
- 	private String specialization;
- 	private int projectWorked=0;
+ private String name;
+ private String specialization;
+ private int projectWorked=0;
 
- 	public Member (String name) {
- 		this.name=name;
- 	}
+ public Member (String name) {
+  this.name=name;
+ }
  	
- 	public Member (String name, String specialization) {
- 		this.name=name;
- 		this.specialization=specialization;
- 	}
+ public Member (String name, String specialization) {
+  this.name=name;
+  this.specialization=specialization;
+ }
  	
- 	public String getName() {
- 		return name;
- 	}
+ public String getName() {
+  return name;
+ }
  	
- 	public void setName(String name) {
- 		this.name = name;
- 	}
+ public void setName(String name) {
+  this.name = name;
+ }
  	
-	public String getSpecialization() {
- 		return specialization;
- 	}
+ public String getSpecialization() {
+  return specialization;
+ }
  	
- 	public int getProjectWorked() {
-		return projectWorked;
- 	}
+ public int getProjectWorked() {
+  return projectWorked;
+ }
  	
-	public void setProjectWorked(int projectWorked) {
-		this.projectWorked=projectWorked;
-	}
+ public void setProjectWorked(int projectWorked) {
+  this.projectWorked=projectWorked;
+ }
  	
- 	public String toString() {
- 		return ("Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" project(s)");
- 	}
+ public String toString() {
+  return ("Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" project(s)");
+ }
 } 
