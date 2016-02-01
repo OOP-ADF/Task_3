8fe3b9c19d9a7eedeb4d10409af07746be3e6da9
adzannugraha <adzan.nugraha@gmail.com>
2016-02-01 23:40:06 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,34 @@
+//IF-38-09_Adzan Nugraha_1301140249
 
+public class AppStore
+{
+		private Application[] appList = new Application[100];
+		private int totalApp = 0;
+		
+		public void createNewApp(String appName, int appSize)
+		{
+			Application apps = new Application();
+			appList[totalApp] = apps;
+			totalApp++;
+		}
+		
+		public Application getApp(int id)
+		{
+			return appList[id];
+		}
+		
+		public int getTotalApp()
+		{
+			return totalApp;
+		}
+		
+		public Application[] getAppList()
+		{
+			return appList;
+		}
+		
+		public String toString()
+		{
+			return("There are "+totalApp+" applications ready to install");
+		}
+}
