038f4ba2be96d26a92f31139b0bc3be7d3ad9381
Malik <malikshi@student.telkomuniversity.ac.id>
2016-02-02 00:30:35 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,20 @@
+// Ibnu Malik Al Kusaeri - 1301140359 - if38-09
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	public String getAppName() {
+		return appName;
+	}
+	public int getAppSize() {
+		return appSize;
+	}
+	public String toString() {
+		return ("Application name : "+appName+", size : "+appSize);
+	}
+}
 
