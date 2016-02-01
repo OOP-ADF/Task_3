10121e65f084bf13eb4519dd44583dda8b6d55db
aristyawirawan <wirawanaristya@gmail.com>
2016-02-02 05:40:24 +0700

    1103134329

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
 
+/**
+ *Aristya Wirawan-1103134329-IF3809
+ * @author aris
+ */
+public class Application {
+    private String appName;
+    private int appSize;
+
+    public Application(String appName, int appSize) {
+        this.appName = appName;
+        this.appSize = appSize;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Application{" + "appName=" + appName + ", appSize=" + appSize + '}';
+    }
+    
+}
--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
 
+/**
+ *Aristya Wirawan-1103134329-IF3809
+ * @author aris
+ */
+public class SmartPhone {
+    private Application[] appList = new Application[10];
+    private int totalApp, memory;
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+    
+    public void addApplication(AppStore appStore,int appId){
+        if (getRemainingSize()>=appStore.getApp(appId).getAppSize()){
+            appList[totalApp]=appStore.getApp(appId);
+            totalApp++;
+        }
+    }
+    public Application getApp(int id){
+        return appList[id];
+    }
+
+    public int getTotalApp() {
+        return totalApp;
+    }
+    public int getRemainingSize(){
+        int x=0;
+        for (int i = 0; i < appList.length; i++) {
+            if (appList[i]!=null) {
+                x=x+appList[i].getAppSize();
+            }
+        }
+        return memory-x;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartPhone{totalApp=" + totalApp + ", memory=" + memory +", remaining memory="+getRemainingSize()+ '}';
+    }
+    
+}
