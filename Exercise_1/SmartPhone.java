58d6852e14d1f62093cd6e3a11d2f02c21a0db3f
FerdyDianAnugrah <ferdykroco01@yahoo.com>
2016-02-02 06:45:58 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,37 @@
+// Ferdy Dian Anugrah(1301140035) IF 3809
+
+public class SmartPhone {
+	
+	private Application[] AppList = new Application[100];
+	private int totalApp = 0;
+	private int memory = 0;
+	
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	
+	public int getRemainingSize() {
+		int temp = 0;
+
+		for(int i = 0; i < AppList.length; i++) {
+			if(AppList[i] != null) {
+				temp = temp + AppList[i].getAppSize();
+			}
+		}
+
+		return memory - temp;
+	}
+	
+	public void addApplication(AppStore appStore, int appId) {
+		Application newApp = appStore.getApp(appId);
+		if(newApp.getAppSize() <= getRemainingSize()) {
+			AppList[totalApp] = newApp;
+			totalApp++;
+		}
+	}
+	
+	public String toString() {
+		return ("memory size "+memory+"MB, "+totalApp+" application installed, remaining memory size : "+getRemainingSize()+"MB");
+	}
+}
 
