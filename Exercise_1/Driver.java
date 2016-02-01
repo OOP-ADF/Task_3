7625adab5dfb9e848d7cda6c7a46a26f4c3ce293
FerdyDianAnugrah <ferdykroco01@yahoo.com>
2016-02-02 06:45:30 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,24 @@
+// Ferdy Dian Anugrah(1301140035) IF 3809
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		AppStore aps = new AppStore();
+		SmartPhone sm = new SmartPhone();
+		aps.createNewApp("app1", 100);
+		aps.createNewApp("app2", 200);
+		aps.createNewApp("app3", 300);
+		aps.createNewApp("app4", 400);
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(1).toString());
+		
+		sm.setMemory(300);
+		System.out.println(sm.toString());
+		
+		sm.addApplication(aps, 1);
+		System.out.println(sm.toString());
+			
+	}
+}
+
 
