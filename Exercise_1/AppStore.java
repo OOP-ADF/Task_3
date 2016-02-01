b296b0d53921cdb5d5fd9f32412950d0387b4227
mhrdandika <mhrdandika@yahoo.com>
2016-02-01 22:52:52 +0700

    Done!

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,22 @@
+//Muhammad Andika
+//1301140145
+//IF 38-09
+ 
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
 
+	public void createNewApp(String appName, int appSize) {
+		Application newApp = new Application(appName, appSize);
+		appList[totalApp] = newApp;
+		totalApp++;
+	}
+
+	public Application getApp(int id) {
+		return appList[id-1];
+	}
+
+	public String toString() {
+		return "There are " + (totalApp) + " applications ready to install";
+	}
+}
--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,25 @@
-
+//Muhammad Andika
+//1301140145
+//IF 38-09
+ 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public String toString() {
+		return ("Application name : "+appName+", with size : "+appSize+"MB");
+	}
+}
