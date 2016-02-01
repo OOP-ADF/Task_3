8b48c7b029316917656abf78c17279de38789232
Luthfi Fauzani <luthfi.f.a15@gmail.com>
2016-02-02 05:39:28 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,26 @@
+//Name : Luthfi Fauzani Ahmad
+//NIM  : 130140029
+//Class: IF-38-09
+
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	
+	public int getAppSize(){
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name : " + appName + " with size : " + appSize);
+	}
+}
 
