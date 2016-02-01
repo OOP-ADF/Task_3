0b23d3cb86bc7abe491b70c32edbc9ec1e74aa86
Malik <malikshi@student.telkomuniversity.ac.id>
2016-02-02 00:29:08 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,20 @@
+// Ibnu Malik Al Kusaeri - 1301140359 - if38-09
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int countApp=0;
+	
+	public void createNewApp(String appName, int appSize) {
+		Application app = new Application(appName, appSize);
+		appList[countApp] = app;
+		countApp++;
+	}
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	public String toString() {
+		return ("There are "+countApp+" applications ready to install");
+	}
+	
+	
+}
 
