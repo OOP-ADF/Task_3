ca23a3889872d03f361e2be9bc9b5b978a38720d
rezamuhammadk <markysemar@gmail.com>
2016-02-01 12:45:19 -0800

    Task_3 selesai

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,37 @@
-
+/**
+ * Nama : Reza Muhammad K
+ * Nim  : 1301144332
+ */
+ 
+public class SmartPhone{
+ 	private Application[] appList = new Application[10];
+ 	private int totalApp;
+ 	private int memory;
+ 	public void setMemory(int memory){
+		this.memory = memory;
+	}
+ 	public void addApplication(AppStore appStore, int appId){
+ 		if(getRemainingSize()>=appStore.getApp(appId).getSizeApp()){
+            totalApp++;
+            appList[totalApp] = appStore.getApp(appId);}
+ 	}
+ 	public Application getApp(int id){
+ 		return appList[id]; 	
+	}
+ 	public int getTotalApp(){
+ 		return totalApp; 	
+	}
+ 	public int getRemainingSize(){
+ 		int temp = 0;
+         int temp2 = memory;
+         for(int i=1;i<appList.length;i++){
+            if(appList[i]!=null) {
+                 temp+=appList[i].getSizeApp();
+			}
+        }
+         return temp2-=temp; 	
+	}
+ 	public String toString (){
+ 		return "memory size "+memory+", "+totalApp+" application installed, remaining memory size: "+getRemainingSize();
+	}
+ }
--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,1 +1,37 @@
-
+/**
+ * Nama : Reza Muhammad K
+ * Nim  : 1301144332
+ */
+ 
+public class Driver{
+	public static void main(String[] args){
+		StartUp stp = new StartUp();
+		Member m = new Member("Alex","Programming");
+		stp.addMember(m);
+		m = new Member("Beni");
+		stp.addMember(m);
+ 		m = new Member("Cakra");
+ 		stp.addMember(m);
+		m = new Member("Deni");
+		stp.addMember(m);
+		m = new Member("Eric");
+ 		stp.addMember(m);
+		stp.createNewProject("project 1");
+ 		stp.createNewProject("project 2");
+		Project p = stp.getProject(0);
+		m = stp.getMember(0);
+		stp.setProjectMember(p, m);
+        p = stp.getProject(1);
+        stp.setProjectMember(p, m);
+         
+        m = stp.getMember(0);
+        p = stp.getProject(0);
+        stp.setProjectMember(p, m);
+		stp.releaseProject(stp.getProject(0));
+		System.out.println(stp.getMember(0).toString());
+		System.out.println(stp.getMember(0).toString());
+		System.out.println(stp.getProject(0).toString());
+		System.out.println(stp.getProject(1).toString());
+		System.out.println("Project released : "+stp.getNumReleasedProject());
+	}
+}
