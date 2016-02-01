178284b8ebaf091f3f707827151ec0029844a112
Luthfi Fauzani <luthfi.f.a15@gmail.com>
2016-02-02 05:40:02 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,31 @@
+//Name : Luthfi Fauzani Ahmad
+//NIM  : 130140029
+//Class: IF-38-09
+
+public class AppStore{
+	private Application[] applist = new Application [100];
+	private int totalApp = 0;
+	
+	public void createNewApp(String appName, int appSize){
+		Application app = new Application(appName, appSize);
+		applist[totalApp] = app;
+		totalApp++;
+	}
+	
+	public Application getApp(int id){
+		return appList[id];
+	}
+	
+	public int getTotalApp(){
+		return totalApp;
+	}
+	
+	public Application[] getAppList(){
+		return appList;
+	}
+	
+	public String toSting(){
+		return ("There are " +  totalApp + " application ready to install");
+	}
+}
 
