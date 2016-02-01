1481c15bf7cc94d9079067f0b7702f06e43ed400
bimamboell <fitrahbiman@yahoo.co.id>
2016-02-02 06:31:54 +0700

    Revert "Revert "a""
    This reverts commit a262e26f820b55159dddb55a03e5e32ed5db8ba1.

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,24 @@
+//Fitrah Bima Nusantara
+//1301140295
+//IF 38-09
 
+public class Driver {
+	
+	public static void main(String[] args) {
+		AppStore aps = new AppStore();
+		SmartPhone sm = new SmartPhone();
+		aps.createNewApp("app1", 100);
+		aps.createNewApp("app2", 200);
+		aps.createNewApp("app3", 300);
+		aps.createNewApp("app4", 400);
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(1).toString());
+		
+		sm.setMemory(300);
+		System.out.println(sm.toString());
+		
+		sm.addApplication(aps, 1);
+		System.out.println(sm.toString());
+			
+	}
+}
