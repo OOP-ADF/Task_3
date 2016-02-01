c25f60c9c54b3d28eb2ccc43459490964462df9b
rizkymuhrifai <rizdec95@gmail.com>
2016-02-01 20:42:22 +0700

    Update StartUp.java

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,55 +1,55 @@
 /*
-	Nama 	: Rizky Muhammad Rifai
-	NIM  	: 1301144099
-	Kelas	: IF-38-09
+Nama 	: Rizky Muhammad Rifai
+NIM  	: 1301144099
+Kelas	: IF-38-09
 */
 
 public class StartUp {
- 	private Member[] member = new Member[100];
- 	private Project[] projectList = new Project[100];
- 	private int nMember=0;
- 	private int nProject=0;
+ private Member[] member = new Member[100];
+ private Project[] projectList = new Project[100];
+ private int nMember=0;
+ private int nProject=0;
  	
- 	public void addMember(Member m) {
- 		member[nMember]=m; 
- 		nMember++;
- 	} 
+ public void addMember(Member m) {
+  member[nMember]=m; 
+  nMember++;
+ } 
  	
- 	public Member getMember(int id) {
- 		return member[id];
- 	}
+ public Member getMember(int id) {
+  return member[id];
+ }
  	
- 	public void createNewProject(String ProjectName) {
- 		Project p=new Project(ProjectName);
- 		projectList[nProject]=p;
- 		nProject++;
- 	}
+ public void createNewProject(String ProjectName) {
+  Project p=new Project(ProjectName);
+  projectList[nProject]=p;
+  nProject++;
+ }
  	
- 	public Project getProject(int id){
- 		return projectList[id];
- 	}
+ public Project getProject(int id){
+  return projectList[id];
+ }
  	
- 	public void  releaseProject(Project p) {
- 		p.releaseApp();
- 	}
+ public void  releaseProject(Project p) {
+  p.releaseApp();
+ }
  	
- 	public int getNumReleasedProject() {
- 		int count;
- 		int i=0;
- 		count=0;
- 		while (projectList[i] != null) {
- 			if (projectList[i].isReleased()) {
- 				count++;
-			}
-			i++;
- 		}
- 		return count;
- 	}
+ public int getNumReleasedProject() {
+  int count;
+  int i=0;
+  count=0;
+ while (projectList[i] != null) {
+  if (projectList[i].isReleased()) {
+   count++;
+  }
+  i++;
+  }
+  return count;
+ }
  	
- 	public void setProjectMember(Project p, Member m) {
- 		int proWork= m.getProjectWorked();
- 		proWork++;
- 		p.addMember(m);
-		m.setProjectWorked(proWork);
- 	}
+ public void setProjectMember(Project p, Member m) {
+  int proWork= m.getProjectWorked();
+  proWork++;
+  p.addMember(m);
+  m.setProjectWorked(proWork);
+ }
 }
