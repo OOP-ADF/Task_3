06d908fcb5357d24960d6e64fa542391f11049ed
Hizas Sabilal Rasyad <hizassr@gmail.com>
2016-02-02 19:17:54 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,37 @@
+/*Hizas Sabilal Rasyad
+  1301140251
+  IF3801
+ */
+public class AppStore
+{
+	private Application[] appList = new Application[100];
+	private  int totalApp = 0;
+	
+	public void createNewApp(String appName, int appSize)
+	{
+		Application newApp = new Application(appName, appSize);
+		//appList[totalApp] = new Application(appName, appSize);
+		appList[totalApp] = newApp;
+		totalApp++;
+	}
 
+	public Application getApp(int id)
+	{
+		return appList[id];
+	}
+	
+	public int getTotalApp()
+	{
+		return totalApp;
+	}
+	
+	public Application[] getAppList()
+	{
+		return appList;
+	}
+	
+	public String toString()
+	{
+		return "There are " + totalApp + " applications ready to install";
+	}
+}
