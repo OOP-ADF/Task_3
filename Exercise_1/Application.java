73df574eb2af13c9a4e933a2f85bb4c1594a4f8c
Ferawidya Primadevi <ferawidya@gmail.com>
2016-02-02 20:58:35 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,23 @@
+//Ferawidya Primadevi//
+//1301140201//
+//IF-38-01//
+
+
+public class Application {
+    private String appName;
+    private int appSize;
+    public Application(String appName, int appSize) {
+        this.appName = appName;
+        this.appSize = appSize;
+    }
+    public String getAppName() {
+        return appName;
+    }
+    public int getAppSize() {
+        return appSize;
+    }
+    public String toString(){
+        return "Application name: "+appName+", with size: "+appSize+" MB";
+    }
+}
 
