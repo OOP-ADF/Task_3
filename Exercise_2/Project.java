3c7f662f8e554c8256147694510dcd6cfb4f1ba6
febbyfeysyah <febbyfeysyah@gmail.com>
2016-02-02 21:47:57 +0700

    Project.java
    Nama : Febby Febriansyah
    Kelas : IF-38-01
    NIM : 1301140371

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,29 @@
+//Nama		: Febby Febriansyah
+//NIM		: 1301140371
+//Kelas		: IF-38-01
 
+public class Project{
+	private String projectName;
+	private Member[] teamMember;
+	private int nTeam = 0;
+	private boolean releaseStatus;
+	
+	public Project(String projectName){
+		this.projectName = projectName;
+		this.teamMember = new Member[5];
+		this.releaseStatus = false; 
+	}
+	public void addMember(Member m){
+		teamMember[nTeam] = m;
+		nTeam++;
+	}
+	public boolean isReleased(){
+		return releaseStatus;
+	}
+	public void releaseApp(){
+		this.releaseStatus = true;
+	}
+	public String toString(){
+		return "Project "+projectName+" status is "+releaseStatus+" with team member of "+nTeam;
+	}
+}
