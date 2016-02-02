0fab53752553f6ef3f058f0c3dc04fa699065e88
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2016-02-02 22:19:06 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
+/* 
+Nama  : Muhammad Yuslan Abubakar
+NIM   : 1301141071
+Kelas : IF-38-01
+*/
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public String toString() {
+		return ("Application name: " + getAppName() + ", with size: " + getAppSize() + "MB");
+	}
+}
