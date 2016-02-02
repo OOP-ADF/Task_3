e47c388c3b894e2072b623b08731e95e4c64cc50
Rizza Aulia Rahman <rizzauliarahman@gmail.com>
2016-02-03 05:58:19 +0700

    Update Driver.java
    Rizza Aulia Rahman (1301144231)
    Kelas : IF-38-01
    
    Update program Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,28 @@
+// Name : Rizza Aulia Rahman
+// Student ID : 1301144231
+// Class : IF-38-01
 
+public class Driver {
+	public static void main (String[] args) {
+		AppStore aps = new AppStore();
+		aps.createNewApp("app1",100);
+		aps.createNewApp("app2",200);
+		aps.createNewApp("app3",300);
+		aps.createNewApp("app4",400);
+		System.out.println(aps.toString());
+		System.out.println("App(s) List");
+		for (int i = 0; i < aps.getTotalApp(); i++) {
+			System.out.println((i+1)+". "+aps.getApp(i).toString());
+		}
+		Smartphone sm = new Smartphone();
+		sm.setMemory(300);
+		System.out.println();
+		System.out.println(sm.toString());
+		sm.addApplication(aps,0);
+		System.out.println(sm.toString());
+		sm.addApplication(aps,2);
+		System.out.println(sm.toString());
+		sm.addApplication(aps,1);
+		System.out.println(sm.toString());
+	}
+}
