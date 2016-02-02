a0746adc48839d43abd3f89f7f206ec4cb3eade3
irfantrianto <irfanttrianto@gmail.com>
2016-02-02 23:38:10 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
+//@auth:Irfan Trianto/1301144111/IF-38-01
 
+public class Application {
+
+	//definisi atribut
+ 	private String appName;
+ 	private int appSize;
+
+ 	//definisi konstruktor
+ 	public Application(String appName, int appSize) {
+ 		this.appName = appName;
+ 		this.appSize = appSize;
+ 	}
+
+ 	//definisi method
+ 	public String getappName() {
+ 		return appName;
+	}
+	
+ 	public int getappSize() {
+ 		return appSize;
+ 	}
+ 	
+ 	public String toString() {
+		return "Application name : "+appName+", with size : "+appSize+"MB";
+	}
+ }
