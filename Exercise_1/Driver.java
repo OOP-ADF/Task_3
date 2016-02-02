4c7845ea18deaf6a3240ad219fb15bc54f6d0f4b
selviwp <selviwidyapraditiap@yahoo.com>
2016-02-02 20:06:53 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,22 @@
+// SELVI WIDYAPRADITIA PANGESTI, 1301144041, IF3801
 
+import java.util.Arrays;
+
+public class Driver {
+    public static void main(String[] args) {
+        AppStore aps = new AppStore();
+        aps.createNewApp("app1", 100);
+        aps.createNewApp("app2", 200);
+        aps.createNewApp("app3", 300);
+        aps.createNewApp("app4", 400);
+        System.out.println(aps.toString());
+        System.out.println(aps.getApp(2).toString());
+        SmartPhone sm = new SmartPhone();
+        sm.setMemory(300);
+        System.out.println(sm.toString());
+        sm.addApplication(aps, 0);
+        sm.addApplication(aps, 1);
+        sm.addApplication(aps, 2);
+        System.out.println(sm.toString());
+    }
+}
