a34cf1a631dff25537b6fd3212a47ead060ceeb3
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2016-02-02 19:16:09 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
+/* 
+  Muhammad Arif Kurniawan
+  1301144091
+  IF-38-01
+*/
+public class Application {
+    private String appName;
+    private int appSize;
+
+    public Application(String appName, int appSize) {
+        this.appName = appName;
+        this.appSize = appSize;
+    }
+    
+    public String getAppName() {
+        return appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+    
+    public String toString(){
+        return "Application name: "+appName+", with size: "+appSize+" MB";
+    }
+}
 
