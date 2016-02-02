766af634c23d93c523fe553ffbe3195fb5124364
yuri liadi <yuriliadi@gmail.com>
2016-02-03 02:22:19 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,19 @@
 
+public class Driver{
+	public static void main(String[] args){
+		AppStore aps=new AppStore();
+		
+		aps.createNewApp("periscope",45);
+		aps.createNewApp("instagram",50);
+		aps.createNewApp("twitter",50);
+		aps.createNewApp("path",65);
+		
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(1).toString());
+		SmartPhone sm=new SmartPhone();
+		sm.setMemory(1000);
+		System.out.println(sm.toString());
+		sm.addApplication(aps,1);
+		System.out.println(sm.toString());
+	}
+}
