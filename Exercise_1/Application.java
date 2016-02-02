4a781f08195e7fab69440642d32cd59e668a55ed
budhipalantra <budhipalantra@ymail.com>
2016-02-02 21:57:29 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
+/* 
+I Nyoman Tri Budhi Palantra
+1301140089
+IF-38-09
+*/
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public String toString() {
+		return ("Application name : "+appName+", size : "+appSize+" MB");
+	}
+}
