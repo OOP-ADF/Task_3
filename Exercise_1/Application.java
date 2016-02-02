cde85641f7896881f188bcab49632b28583ea5cd
Rizza Aulia Rahman <rizzauliarahman@gmail.com>
2016-02-03 05:56:35 +0700

    Update Application.java
    Rizza Aulia Rahman (1301144231)
    Kelas : IF-38-01
    
    Update program Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,23 @@
+// Name : Rizza Aulia Rahman
+// Student ID : 1301144231
+// Class : IF-38-01
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	public String getAppName() {
+		return appName;
+	}
+	public int getAppSize() {
+		return appSize;
+	}
+	public String toString() {
+		String a = "Application name : "+appName+", with size : "+appSize+" MB";
+		return a;
+	}
+}
