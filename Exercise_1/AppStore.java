d1a30b22d0540b8dc5637483d342bdbe66b0dfea
Reza Aryadi <aryav.reza08@gmail.com>
2016-02-02 15:41:04 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,26 @@
-
+/* Reza Aryadi 1301144179 - IF 38 09 */
+public class AppStore{
+	private Application appList[] = new Application[100];
+	private int totalApp = 0;
+	public void createNewApp(String appName, int appSize){
+		Application a = new Application(appName, appSize);
+		appList[totalApp] = a;
+		totalApp++;
+	}
+	
+	public Application getApp(int id){
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public Application[] getAppList(){
+		return appList;
+	}
+	
+	public String toString(){
+		return ("There are "+totalApp+" applications ready to install.");
+	}
+}		
