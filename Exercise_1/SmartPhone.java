1352abfe590e0a0950a8d5c8d2c44294abbd8af1
adrianurcahyo <adriangusti@hotmail.com>
2016-02-03 00:40:30 +0700

    Adrian G Nurcahyo
    1301140092
    IF-38-02

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,48 @@
+//Adrian G. Nurcahyo
+//1301140092
+//IF-38-02
 
+public class SmartPhone 
+{
+	private Application[] appList = new Application[10];
+	private int totalApp = 0;
+	private int memory;
+	
+	public void setMemory(int memory) 
+	{
+		this.memory = memory;
+	}
+	public void addApplication(AppStore appStore, int appId) 
+	{
+		if(getRemainingSize()>=appStore.getApp(appId).getAppSize()) 
+		{
+            appList[totalApp] = appStore.getApp(appId);
+			totalApp++;
+        }
+	}
+	public Application getApp(int id) 
+	{
+		return appList[id];
+	}
+	public int getTotalApp() 
+	{
+		return totalApp;
+	}
+	public int getRemainingSize() 
+	{
+		int a = 0;
+        int temp = memory;
+        for(int k=0;k<appList.length;k++) 
+		{
+            if(appList[k]!=null) 
+			{
+                a+=appList[k].getAppSize();
+            }
+        }
+        return temp-=a;
+	}
+	public String toString () 
+	{
+		return "memory size "+memory+", "+totalApp+" application installed, remaining memory size: "+getRemainingSize();
+	}
+}
--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,56 @@
+//Adrian G. Nurcahyo
+//1301140092
+//IF-38-02
 
+public class StartUp 
+{
+	private Member[] member = new Member[50];
+	private Project[] projectList = new Project[50];
+	private int nMember = 0;
+	private int nProject = 0;
+	
+	public void addMember(Member m) 
+	{
+		member[nMember] = m;
+        nMember++;
+	}
+	public Member getMember(int id) 
+	{
+		return member[id];
+	}
+	public void createNewProject(String projectName) 
+	{
+		projectList[nProject] = new Project(projectName);
+		nProject++;
+	}
+	public Project getProject(int id) 
+	{
+		return projectList[id];
+	}
+	public void setProjectMember(Project p, Member m) 
+	{
+        int i=m.getprojectWorked();
+        i++;
+		p.addMember(m);
+        m.setprojectWorked(i);
+	}
+	public void releaseProject(Project p) 
+	{
+		p.releaseApp();
+	}
+	public int getNumReleasedProject() 
+	{
+        int count = 0;
+		for(int i=0;i<projectList.length;i++) 
+		{
+            if(projectList[i]!=null) 
+			{
+                if(projectList[i].isReleased()) 
+				{
+                    count++;
+                }
+            }
+        }
+        return count;
+	}
+}
