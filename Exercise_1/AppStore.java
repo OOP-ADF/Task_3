5aa75766d9a3e15b9a194c63a6cd52982aa5747d
Hafidrozaq <syahrizalrozaqi@gmail.com>
2016-02-02 21:12:03 +0700

    SYAHRIZAL HAFID ROZAQI 1301144252 IF3802

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,37 @@
+// Syahrizal Hafid Rozaqi IF-38-02 1301144252
 
+public class AppStore {
+	private Application[] appList;
+	private int totalApp;
+	
+	public AppStore() {
+		appList = new Application[100];
+		totalApp = 0;
+	}
+	
+	public void createNewApp(String appName, int appSize) {
+		Application app = new Application(appName, appSize);
+		appList[getTotalApp()] = app;
+	}
+	
+	public int getTotalApp() {
+		totalApp = 0;
+		for(Application app: appList) {
+			if(app != null)
+				totalApp++;
+		}
+		return totalApp;
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return "There are " + getTotalApp() + " apps available!";
+	}
+}
--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,1 +1,37 @@
+// Syahrizal Hafid Rozaqi IF-38-02 1301144252
 
+public class Driver {
+	public static void main(String[] args) {
+		StartUp stp = new StartUp();
+		Member m = new Member("Alex","Gaming");
+		stp.addMember(m);
+		m = new Member("Beni");
+		stp.addMember(m);
+		m = new Member("Cakra");
+		stp.addMember(m);
+		m = new Member("Deni");
+		stp.addMember(m);
+		m = new Member("Eric","Masak");
+		stp.addMember(m);
+		stp.createNewProject("project 1");
+		stp.createNewProject("project 2");
+        
+        Project p = stp.getProject(0);
+        m = stp.getMember(0);
+        stp.setProjectMember(p, m);
+        p = stp.getProject(1);
+        stp.setProjectMember(p, m);
+        
+        m = stp.getMember(4);
+        p = stp.getProject(0);
+        stp.setProjectMember(p, m);
+        
+        stp.releaseProject(stp.getProject(0));
+        
+        System.out.println(stp.getMember(0).toString());
+        System.out.println(stp.getMember(4).toString());
+        System.out.println(stp.getProject(0).toString());
+        System.out.println(stp.getProject(1).toString());
+        System.out.println("Project released : "+stp.getNumReleasedProject());
+	}
+}
