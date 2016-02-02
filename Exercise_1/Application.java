5967424c950d5f2ee9ed7d4f47ff9c6a7e0c91d6
Ari Adrian Ibrahim <ariadrian.i@gmail.com>
2016-02-02 20:28:00 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,25 @@
-
+public class Application{
+ 	private String appName;
+ 	private int appSize;
+ 	
+ 	public Application(String appName, int appSize){
+ 		this.appName = appName;
+ 		this.appSize = appSize;
+ 	}
+ 	
+ 	public String getAppName(){
+ 		return appName;
+	}
+	
+ 	public int getAppSize(){
+ 		return appSize;
+ 	}
+ 	
+ 	public String toString(){
+ 		return "Application Name : " + appName + ", with size : " + appSize + " MB";
+ 	}
+ }
+ 
+//Ari Adrian Ibrahim
+//1301142301
+//IF 38 01
