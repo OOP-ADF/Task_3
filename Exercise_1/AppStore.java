d6c43e526818565697e8dd1eb7b0e0be5c5e13a3
Dhiva Azhara <dhivazhr@gmail.com>
2016-02-02 22:53:33 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,19 @@
+//DHIVA AZHARA (1301144101) IF-38-01
+
+public class AppStore {
+   private Application[] appList = new Application[100];
+   private int totalApp=0; 
+   
+   public void createNewApp(String appName, int appSize) {
+       Application a = new Application(appName,appSize);
+       appList[totalApp] = a;
+       totalApp++;
+   }
+   public Application getApp(int id) {
+       return appList[id];
+   }
+   public String toString() {
+       return("There are "+totalApp+" applications ready to install");
+   }
+}
 
