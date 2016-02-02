074907f84be1f61a47a05ea23dca0d8aee2d2c70
Thoriq Abdul Aziz <riq.aziz@gmail.com>
2016-02-02 08:08:41 +0700

    Update Application.java
    /* Thoriq Abdul Aziz
    	1301144341
    	IF-38-01
    */

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,26 @@
+/* Thoriq Abdul Aziz
+	1301144341
+	IF-38-01
+*/
+public class Application {
+    private String appName;
+    private int appSize;
+    
+    Application(String appName, int appSize){
+        this.appName = appName;
+        this.appSize = appSize;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+
+    public String toString(){
+        return "Application name: "+appName+", with size: "+appSize;
+    }
+}
 
