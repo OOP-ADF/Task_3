318bcf8e50a859b6b67a4c7cc4edc69b1b3e48df
Rajasaurs <rajaryanpradana@yahoo.co.id>
2016-02-02 23:26:41 +0700

    Task 3
    Raja Ryan Pradana
    1301140302
    IF-38-02

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,45 @@
-
+/*
+Name 	: Raja Ryan Pradana
+NIM 	: 1301140302
+Kelas 	: IF-38-02
+*/
+public class SmartPhone {
+	private Application []appList = new Application[10];
+	private int totalApp;
+	private int memory;
+	
+	
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	
+	public void addApplication(AppStore appStore, int appid) {
+		if(getRemainingSize()>=appStore.getApp(appid).getappSize()) {
+            appList[totalApp] = appStore.getApp(appid);
+			totalApp++;
+		}
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public int getRemainingSize() {
+		int x = 0;
+        int temp = memory;
+        for(int i=0;i<appList.length;i++) {
+            if(appList[i]!=null) {
+                x+=appList[i].getappSize();
+            }
+        }
+        return temp-=x;
+	}
+	
+	public String toString() {
+		return "memory size "+memory+", "+totalApp+" application installed, remaining memory size: "+getRemainingSize();
+	}
+}
