c374f9f25eeb1890b09b98d7a48f1ee3550e9513
aliyanurr <aliyanurr@gmail.com>
2016-02-02 21:39:09 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,32 @@
+//Aliya Nur Rezkita (1301144161)
+//IF-38-01
 
+public class AppStore {
+
+    private Application[] appList=new Application[100];
+    private int totalApp;
+
+    public void createNewApp(String appName, int appSize) {
+        Application p = new Application(appName, appSize);
+        appList[totalApp]=p;
+        totalApp++;
+
+    }
+
+    public Application getApp(int id) {
+        return appList[id];
+    }
+
+    public int getTotalApp() {
+        return totalApp;
+    }
+
+    public Application[] getAppList() {
+        return appList;
+    }
+
+    @Override
+    public String toString() {
+        return "There are " + getTotalApp() + " applications ready to install";
+    }
+}
