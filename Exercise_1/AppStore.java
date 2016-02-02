5119949150cb8b43a6be752bdb24f2b1240b922e
Adam Geraldy <adam.geraldy@gmail.com>
2016-02-02 20:17:18 +0700

    AppStore done

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,37 @@
+/*Nama: Adam Geraldy Katab
+Kelas: IF-38-01
+NIM: 1301140381*/
 
+public class AppStore {
+	private Application[] appList;
+	private int totalApp = 0; // redundant?
+	
+	public AppStore() {
+		appList = new Application[100];
+	}
+	
+	public void createNewApp(String appName, int appSize) {
+		Application app = new Application(appName, appSize);
+		appList[getTotalApp()] = app;
+	}
+	
+	public int getTotalApp() {
+		for(Application app: appList) {
+			if(app != null)
+				totalApp++;
+		}
+		return totalApp;
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return "There are " + getTotalApp() + " apps available!";
+	}
+}
