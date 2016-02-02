1e17693ad15ad9b420e609fac82848d99fb66a4d
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2016-02-02 19:15:32 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,37 @@
+/* 
+  Muhammad Arif Kurniawan
+  1301144091
+  IF-38-01
+*/
+public class AppStore {
 
+    private Application[] appList=new Application[100];
+    private int totalApp;
+
+    public void createNewApp(String appName, int appSize) {
+        Application p = new Application(appName, appSize);
+        appList[totalApp]=p;
+        totalApp++;
+
+    }
+
+    public Application getApp(int id) {
+        return appList[id];
+    }
+
+    public int getTotalApp() {
+        return totalApp;
+    }
+
+    public Application[] getAppList() {
+        int i=0;
+        while(appList[i]!=null){
+            
+        }
+        return appList;
+    }
+
+    public String toString() {
+        return "There are " + getTotalApp() + " applications ready to install";
+    }
+}
