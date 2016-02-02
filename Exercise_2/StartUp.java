8590b159d542452c9bf512f333ba32263d42394c
Adam Geraldy <adam.geraldy@gmail.com>
2016-02-02 21:31:55 +0700

    Startup done

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,54 @@
+/*Nama: Adam Geraldy Katab
+Kelas: IF-38-01
+NIM: 1301140381*/
 
+public class StartUp {
+	private Member[] member;
+	private Project[] projectList;
+	private int nMember;
+	private int nProject;
+	
+	public StartUp() {
+		member = new Member[50];
+		projectList = new Project[50];
+		nMember = 0;
+		nProject = 0;
+	}
+	
+	public void addMember(Member m) {
+		member[nMember] = m;
+		nMember++;
+	}
+	
+	public Member getMember(int id) {
+		return member[id];
+	}
+	
+	public void createNewProject(String projectName) {
+		Project new_project = new Project(projectName);
+		projectList[nProject] = new_project;
+		nProject++;
+	}
+	
+	public Project getProject(int id) {
+		return projectList[id];
+	}
+	
+	public void setProjectMember(Project p, Member m) {
+		p.addMember(m);		
+	}
+	
+	public void releaseProject(Project p) {
+		p.releaseApp();
+	}
+
+	public int getNumReleasedProject() {
+		int count_released = 0;
+		for(Project proj: projectList) {
+			if(proj != null) {
+				if(proj.isReleased()) count_released++;
+			}
+		}
+		return count_released;
+	}
+}
