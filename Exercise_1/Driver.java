d264c7f0afee2f95813bced59bda8b8f66113234
riksameidy <riksameidy@gmail.com>
2016-02-03 06:12:27 +0700

    [IF-38-09][1301142289]

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,33 @@
+/*
+	Riksa Meidy Karim
+	1301142289
+	IF-38-09
+*/
 
+public class Driver{
+	public static void main(String[] args){
+		AppStore apps = new AppStore();
+		apps.createNewApp("ap1",50);
+		apps.createNewApp("ap2",150);
+		apps.createNewApp("ap3",250);
+		apps.createNewApp("ap4",350);
+		System.out.println(apps.toString());
+		System.out.println(apps.getApp(3).toString());
+		
+		SmartPhone s = new SmartPhone();
+		s.setMemory(1000);
+		System.out.println(s.toString());
+		s.addApplication(apps, 0);
+		s.addApplication(apps, 1);
+		s.addApplication(apps, 2);
+		System.out.println(s.toString());
+		
+		//my scenario
+		SmartPhone r = new SmartPhone();
+		r.setMemory(500);
+		r.addApplication(apps, 3);
+		r.addApplication(apps, 2);
+		System.out.println(r.toString());
+		
+	}
+}
--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,43 @@
+/*
+	Riksa Meidy Karim
+	1301142289
+	IF-38-09
+*/
 
+public class Project{
+	private String projectName;
+	private Member teamMember[];
+	private int nTeam;
+	private boolean releaseStatus;
+	
+	public Project(String projectName){
+		this.projectName = projectName;
+		teamMember = new Member[5];
+		releaseStatus = false;
+		nTeam = 0;
+	}
+	
+	public void addMember(Member m){
+		if(nTeam<5){
+		teamMember[nTeam] = m;
+		nTeam++;
+		m.setProjectWorked(m.getProjectWorked() + 1);
+		}
+		else{
+			System.out.println("Team Member is full");
+		}
+	}
+	public boolean isReleased(){
+		return releaseStatus;
+	}
+	public void releaseApp(){
+		releaseStatus = true;
+	}
+	public String toString(){
+		if(releaseStatus==false){
+		return "Project " + projectName+ " is in progress with team member of " + Integer.toString(nTeam);
+		}
+		else{return "Project " + projectName+ " is already released with team member of " + Integer.toString(nTeam);}
+	}
+	
+}
