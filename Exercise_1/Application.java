8cb210464a6fc5ba90d34d74df02d580e941f768
febbyfeysyah <febbyfeysyah@gmail.com>
2016-02-02 21:43:34 +0700

    Application.java
    Nama : Febby Febriansyah
    Kelas : IF-38-01
    NIM : 1301140371

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,22 @@
+//Nama		: Febby Febriansyah
+//NIM		: 1301140371
+//Kelas		: IF-38-01
 
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName,int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	public String getappName(){
+		return appName;
+	}
+	public int getappSize(){
+		return appSize;
+	}
+	public String toString(){
+		return "Application name : "+appName+", with size : "+appSize+"MB";
+	}
+}
