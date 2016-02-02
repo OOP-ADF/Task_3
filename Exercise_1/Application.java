19d3035ea8b3b60bbc7e4da08c78cb46bd86e4d0
Deta Kurnia Soundra <dhetasondra@gmail.com>
2016-02-02 23:50:06 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,23 @@
-
+/* Deta Kurnia Soundra 1301140109 - IF 38 09 */
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public  Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getappName() {
+		return appName;
+	}
+	
+	public int getappSize() {
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name: "+appName+", Size: "+appSize);
+	}
+	
+}
