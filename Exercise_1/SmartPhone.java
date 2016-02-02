019505f1ba1aba0ff0461856c3d4a43052c58fe5
irfantrianto <irfanttrianto@gmail.com>
2016-02-02 23:39:25 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,43 @@
+//@auth:Irfan Trianto/1301144111/IF-38-01
 
+public class SmartPhone {
+
+	//definisi atribut
+	private Application[] appList =  new Application[10];
+	private int totalApp = 0;
+	private int memory;
+	
+	//definisi method
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	
+	public void addApplication(AppStore appStore, int appId) {
+		if(appStore.getApp(appId).getappSize() < getRemainingSize()) {
+			appList[totalApp] = appStore.getApp(appId); 
+			totalApp++;
+		}
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public int getRemainingSize() {
+		int a = 0;
+		if(getTotalApp() > 0) {
+			for(int i = 0; i < getTotalApp(); i++) {
+				a += appList[i].getappSize();
+			}
+		}
+		return memory - a;
+	}
+	
+	public String toString() {
+		return "Memory Size " + getRemainingSize() + ", " + getTotalApp() + " application installed, remaining memory size : " +  getRemainingSize() + " MB";
+	}
+}
