f36966b3e90c8c85589c7e20ade85687cef4c7aa
Alfian Pamungkas Sakawiguna <admin@idcloudhost.com>
2016-02-02 23:32:04 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,25 @@
+// NAMA : Alfian Pamungkas Sakawiguna
+// KELAS : IF-38-01
+// NIM : 1301144411
 
+public class AppStore {
+	private Application[] appList=new Application[100];
+	private int totalApp=0;
+	public void createNewApp(String appName, int appSize) {
+    Application app=new Application(appName,appSize);
+    appList[totalApp]=app;
+    totalApp++;
+  }
+	public Application getApp(int id){
+		return appList[id];
+	}
+	public int getTotalApp(){
+		return totalApp;
+	}
+	public Application[] getAppList(){
+		return appList;
+	}
+	public String toString() {
+		return ("There are "+getTotalApp()+"applications ready to install");
+	}
+}
