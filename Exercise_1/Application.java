f9fde34a5f1981fe754a3ec128750f15a757ebfb
gctatmaja <genturcta@gmail.com>
2016-02-03 06:46:50 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,33 @@
+/*
+Gentur Cipto Tri Atmaja
+1301140239
+IF 38-09
+ */
+
+public class Application 
+{
+    private String appName;
+    private int appSize;
+    
+    public Application(String appName, int appSize)
+    {
+       this.appName = appName;
+       this.appSize = appSize;
+    }
+
+    public String getAppName() 
+    {
+        return appName;
+    }
+    
+    public int getAppSize() 
+    {
+        return appSize;
+    }
+
+    public String toString()
+    {
+        return " Aplikasi " + appName + " mempunyai ukuran " + appSize + "MegaByte";
+    }
+}
 
