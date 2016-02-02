d1a020f16c137e16035eac24e13e5c68a9c2bc01
fadilahramadhan <fadilah.gudil@gmail.com>
2016-02-02 23:11:41 +0700

    //Akhmad Fadilah Ramadhan 1301140222 IF-38-02
    //Akhmad Fadilah Ramadhan 1301140222 IF-38-02

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,27 @@
-
+public class AppStore {
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+	
+	public void createNewApp(String appName, int appSize) {
+		Application apps = new Application(appName, appSize);
+		appList[totalApp] = apps;
+		totalApp++;
+	}
+	
+	public Application getApp(int id) {
+		return appList[id];
+	}
+	
+	public int getTotalApp() {
+		return totalApp;
+	}
+	
+	public Application[] getAppList() {
+		return appList;
+	}
+	
+	public String toString() {
+		return ("There are "+getTotalApp()+" applications ready to install");
+	}
+}
+//AKHMAD FADILAH RAMADHAN	1301140222
