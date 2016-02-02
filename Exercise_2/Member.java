41eed2c0d4486fa7675d57dcdf653732481b4500
febbyfeysyah <febbyfeysyah@gmail.com>
2016-02-02 21:47:16 +0700

    Member.java
    Nama : Febby Febriansyah
    Kelas : IF-38-01
    NIM : 1301140371

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,38 @@
+//Nama		: Febby Febriansyah
+//NIM		: 1301140371
+//Kelas		: IF-38-01
 
+public class Member{
+	private String name,specialization;
+	private int projectWorked = 0;
+	
+	public Member(String name){
+		setName(name);
+	}
+	public Member(String name,String specialization){
+		setName(name);
+		setSpecialization(specialization);
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setSpecialization(String specialization){
+		this.specialization = specialization;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getSpecialization(){
+		return specialization;
+	}
+	public String toString(){
+		return "Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" project(s)";
+	}
+	public void setProjectWorked(int projectWorked){
+		this.projectWorked = projectWorked;
+	}
+	public int getProjectWorked(){
+		projectWorked++;
+		return projectWorked;
+	}
+}
