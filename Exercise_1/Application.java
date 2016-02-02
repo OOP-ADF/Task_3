402aff5bc85a0a1a871eff7360cc49b8d71b6875
Adam Geraldy <adam.geraldy@gmail.com>
2016-02-02 20:16:55 +0700

    Application.java done

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,24 @@
+/*Nama: Adam Geraldy Katab
+Kelas: IF-38-01
+NIM: 1301140381*/
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize) {
+		
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	
+	public int getAppSize() {
+		return appSize;
+	}
+	
+	public String toString() {
+		return "Application name: " + appName + ", size: " + appSize + "MB";
+	}
+}
