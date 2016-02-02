aacb77a5fe56108a249ade72d004f2625bfded2c
WindyJihan <windyjihan2010@gmail.com>
2016-02-02 08:55:48 -0800

    Update Driver.java

--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,1 +1,36 @@
+/*Nama	: Windy Israniati Jihan
+NIM		: 1301144309
+Kelas	: IF-38-09 */
+
+public class Driver {
+	public static void main (String[] args) {
+		StartUp stp = new StartUp();
+		
+		Member m = new Member("Alex");
+		stp.addMember( m );
+		Member m = new Member("Beni");
+		stp.addMember( m );
+		Member m = new Member("Cakra");
+		stp.addMember( m );
+		Member m = new Member("Deni");
+		stp.addMember( m );
+		Member m = new Member("Eric");
+		stp.addMember( m );
+		
+		stp.createNewProject( "project1" );
+		stp.createNewProject( "project2" );
+		
+		Project p = stp.getProject( 0 );
+		Member m = stp.getMember( 0 );
+		stp.setProjectMember( p , m );
+		
+		stp.releaseProject( stp.getProject( 1 ) );
+		System.out.println( stp.getMember( 0 ).toString() );
+		System.out.println( stp.getProject( 1 ).toString() );
+		System.out.println( stp.getProject( 2 ).toString() );
+		System.out.println( stp.getProject( 3 ).toString() );
+		System.out.println( stp.getProject( 4 ).toString() );
+		System.out.println("Project Number released = "+stp.getNumReleasedProject());
+	}
+}
 
