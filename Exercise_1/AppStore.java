21da3ed2da2fe35489da9355b73901a047d5abda
Ari Adrian Ibrahim <ariadrian.i@gmail.com>
2016-02-02 20:26:01 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,30 @@
-
+public class AppStore{
+ 	private Application[] appList = new Application[100];
+ 	private  int totalApp = 0;
+ 	
+ 	public void createNewApp(String appName, int appSize) {
+ 		Application newApp = new Application(appName, appSize);
+ 		appList[totalApp] = newApp;
+ 		totalApp++;
+ 	}
+	
+	public int getTotalApp(){
+ 		return totalApp;
+ 	}
+  
+ 	public Application getApp(int id){
+ 		return appList[id];
+ 	}
+ 	
+ 	public Application[] getAppList(){
+ 		return appList;
+ 	}
+ 	 
+ 	public String toString(){
+ 		return "There are " + totalApp + " applications ready to install";
+ 	}
+ }
+ 
+ //Ari Adrian Ibrahim
+ //1301142301
+ //IF 38 01
