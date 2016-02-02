8b92c723f5916251ae85af7fb7fd784c4a0af321
I Komang Hendra Wijaya Kusuma <ndrawk@gmail.com>
2016-02-03 00:49:31 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,28 @@
+//[IF-38-09]_1301144139_I Komang Hendra Wijaya Kusuma
+
+public class AppStore{
+	private Application[] appList = new Application[100];
+	private int totalApp=0;
+	
+	public void createNewApp(String appName, int appSize){
+		Application app= new Application(appName,appSize);
+		appList[totalApp]= app;
+		totalApp++;
+	}
+	
+	public Application getApp(int id){
+		return appList[id];
+	}
+	public int getTotalApp(){
+		return totalApp;
+	}
+	
+	public Application[] getlAppList(){
+		return appList;		
+	}
+	
+	public String toString(){
+		return "There are "+totalApp+" application ready to install";
+	}
+}
 
