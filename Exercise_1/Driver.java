96a13a2845ff3f1fac943c9a825580cdfee30fba
Atikawahyu <awahyugirini@gmail.com>
2016-02-03 15:12:18 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,17 @@
 
+//atika w - 1301144211
+public class Nom1 {
+	
+	public static void main(String [] args){
+	AppStore a = new AppStore();
+	a.createNewApp("app1",100);
+	a.createNewApp("app2",270);
+	a.createNewApp("app3",88);
+	System.out.println(a.toString());
+	SmartPhone s= new SmartPhone();
+	s.setMemory(500);
+	
+	s.addApplication(a, 0);
+	System.out.println(s.toString());
+	}
+}
