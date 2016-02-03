4be539b89ec49660a58a0834d4eea3e22d412d51
andhikagilang <dhikagil@gmail.com>
2016-02-04 00:52:49 +0900

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,24 @@
+// Nama : Andhika Gilang K
+// Kelas : IF-38-01
+// NIm : 1301140361
 
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName,int appSize) {
+		this.appName=appName;
+		this.appSize=appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	public int getAppSize(){
+		return appSize;
+	}
+	
+	public String toString() {
+		return ("Application name : "+appName+"with size : "+appSize);
+	}
+}
