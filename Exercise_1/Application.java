565c67d6010413c6a4bc0458879132954102aa72
friendlysipayung <friendly.1301144001@gmail.com>
2016-02-03 08:25:54 +0700

    Application.java
    Nama : Friendly Halomoan Sipayung 
    NIM : 1301144001
    Kelas : IF-38-01

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,21 @@
+public class Application {
+    private String appName;
+    private int appSize;
 
+    public Application(String appName, int appSize) {
+        this.appName = appName;
+        this.appSize = appSize;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+    
+    public String toString(){
+        return ("Application name: " + getAppName() + ", with size: " + getAppSize() + "MB");
+    }
+}
