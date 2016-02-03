823ed3d31be499504d4714fece2cf438743d939b
Atikawahyu <awahyugirini@gmail.com>
2016-02-03 15:10:24 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,23 @@
-
+//atika w -1301144211
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public Application( String appName,int appSize){
+		this.appName=appName;
+		this.appSize=appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	
+	public int getAppSize(){
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name: "+this.appName+" with size: "+this.appSize+" MB");
+	}
+	
+}
