15d697bec8588e8e9a7ebe27488128809628afca
friendlysipayung <friendly.1301144001@gmail.com>
2016-02-03 23:58:03 +0700

    Driver.java
    Nama : Friendly Halomoan Sipayung 
    NIM : 1301144001
    Kelas : IF-38-01

--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -2,29 +2,29 @@ public class Driver {
     public static void main(String[] args) {
         StartUp stp = new StartUp();
         Member m1 = new Member("Alex","Design");
-	      stp.addMember(m1);
-	      Member m2 = new Member("Beni");
-	      stp.addMember(m2);
-	      Member m3 = new Member("Cakra");
-	      stp.addMember(m3);
-	      Member m4 = new Member("Deni");
-	      stp.addMember(m4);
-	      Member m5 = new Member("Eric");
-	      stp.addMember(m5);
+	stp.addMember(m1);
+	Member m2 = new Member("Beni");
+	stp.addMember(m2);
+	Member m3 = new Member("Cakra");
+	stp.addMember(m3);
+	Member m4 = new Member("Deni");
+	stp.addMember(m4);
+	Member m5 = new Member("Eric");
+	stp.addMember(m5);
 	
-	      stp.createNewProject("Project1");
-	      stp.createNewProject("Project2");
+	stp.createNewProject("Project1");
+	stp.createNewProject("Project2");
 	
-	      Project p1 = stp.getProject(0);
-	      Project p2 = stp.getProject(1);
+	Project p1 = stp.getProject(0);
+	Project p2 = stp.getProject(1);
 	
-	      stp.setProjectMember(p1,m1);
-	      stp.setProjectMember(p1,m2);
-	      stp.setProjectMember(p1,m4);
-	      stp.setProjectMember(p2,m3);
-	      stp.setProjectMember(p2,m5);
+	stp.setProjectMember(p1,m1);
+	stp.setProjectMember(p1,m2);
+	stp.setProjectMember(p1,m4);
+	stp.setProjectMember(p2,m3);
+	stp.setProjectMember(p2,m5);
 	
-	      stp.releaseProject(stp.getProject(1));
+	stp.releaseProject(stp.getProject(1));
         		
       	System.out.println(stp.getMember(0).toString());
       	System.out.println(stp.getProject(0).toString());
