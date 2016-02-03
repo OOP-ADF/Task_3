eabfa6ac8a9785c9eb417b71c3c3e32740a2179d
suhendaragung20 <suhendaragung20@gmail.com>
2016-02-03 08:15:00 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,22 @@
+//Agung Suhendar_1301140279_IF 38-09
 
+public class Application{
+	private String appName;
+	private int appSize;
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getName(){
+		return appName;
+	}
+	
+	public int getSize(){
+		return appSize;
+	}
+	
+	public String toString(){
+		return "Application name: "+appName+", with size: "+appSize+"MB";
+	}
+}
