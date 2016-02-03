0f581636db4d47e4debc3d17acc04369446003ab
akbarm23 <akbar.syahidul@gmail.com>
2016-02-03 07:44:40 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,38 @@
+//Syahidul Akbar Mardhotillah (1301140331)
 
+public class Project{
+	private String projectName;
+	private Member[] teamMember = new Member[5];
+	private int nTeam = 0;
+	private boolean releaseStatus = false;
+	
+	public Project(String projectName){
+		this.projectName = projectName;
+	}
+	
+	public void addMember(Member m){
+		teamMember[nTeam] = m;
+		nTeam++;
+		m.setProjectWorked(m.getProjectWorked()+1);
+	}
+	
+	public boolean isReleased(){
+		return releaseStatus;
+	}
+	
+	public void releaseApp(){
+		releaseStatus = true;
+	}
+	
+	public String toString(){
+		String x = "";
+		if(releaseStatus == true){
+		x = ("Project "+projectName+" Status is complete with team member of "+nTeam);}
+			else
+			{
+				x = ("Project "+projectName+" Status is in progress with team member of "+nTeam);
+			}
+		
+		return x;
+	}
+}
