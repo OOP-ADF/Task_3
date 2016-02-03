0e413d42b3b2db4e24659b1e912b54d3b16a3050
suhendaragung20 <suhendaragung20@gmail.com>
2016-02-03 11:01:27 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,44 @@
+//Agung Suhendar_1301140279_IF 38-09
 
+public class Member{
+	private String name;
+	private String specialization;
+	private int projectWorked = 0;
+	
+	public Member(String name){
+		this.name = name;
+	}
+	
+	public Member(String name,String specialization){
+		this.name = name;
+		this.specialization = specialization;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setSpecialization(String specialization){
+		this.specialization = specialization;
+	}
+	
+	public String getSpecialization(){
+		return specialization;
+	}
+	
+	public void setProjectWorked(int projectWorked){
+		this.projectWorked = projectWorked;
+	}
+	
+	public int getProjectWorked(){
+		return projectWorked;
+	}
+	
+	public String toString(){
+		return "Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" projec(s)";
+	}
+}
