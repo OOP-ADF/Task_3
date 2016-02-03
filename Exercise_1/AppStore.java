a59937df63272e5a0e251a39fa39db106a1bb44f
friendlysipayung <friendly.1301144001@gmail.com>
2016-02-03 08:24:48 +0700

    AppStore.java
    Nama : Friendly Halomoan Sipayung 
    NIM : 1301144001
    Kelas : IF-38-01

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,26 @@
-
+public class AppStore {
+    private Application[] appList = new Application[100];
+    private int totalApp;
+    
+    public void createNewApp (String appName, int appSize){
+        Application apps = new Application(appName, appSize);
+        appList [totalApp] = apps;
+        totalApp++;
+    }
+    
+    public Application getApp(int id) {
+        return appList[id];
+    }
+    
+    public int getTotalApp() {
+        return totalApp;
+    }
+    
+    public Application[] getAppList() {
+	return appList;
+    }
+    
+    public String toString() {
+	return ("There are " + getTotalApp() + " applications ready to install");
+    }
+}
