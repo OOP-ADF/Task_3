d4eba5f106db3e4231c2ce25d12082e9b517c16f
yulizar <erwinyulizar14@gmail.com>
2016-02-03 11:50:32 +0700

    Update StartUp.java

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,48 @@
+/*
+Name : Erwin Yulizar F
+NIM  : 1301144031
+Class: IF-38-01
+*/
 
+public class StartUp{
+	private Member[] member;
+	private Project[] projectList;
+	private int nMember = 0;
+	private int nProject = 0;
+	
+	public void addMember(Member m){
+		member[nMember] = m;
+		nMember++;
+	}
+	
+	public Member getMember (int id){
+		return member[id];
+	}
+	
+	public void createNewProject(String projectName){
+		Project newProject = new Project(projectName);
+		projectList[nProject] = newProject;
+		nProject++;
+	}
+	
+	public void setProjectMember(Project p, Member m){
+		p.addMember(m);
+	}
+	
+	public Project getProject(int id){
+		return projectList[id];
+	}
+	
+	public void releaseProject(Project p){
+		p.releaseApp();
+	}
+	
+	public int getNumReleasedProject(){
+		return nProject;
+	}
+	
+	public int getAllMember(){
+		return nMember;
+	}
+	
+}
