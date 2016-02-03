e97f74671aafcd64b9bb913cb53f3309ffd947d9
primaananda <emailiputuprimaananda@gmail.com>
2016-02-03 19:25:52 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,24 @@
+/*
+*name  : I Putu Prima Ananda
+*class : IF-38-01
+*NIM   : 1301144291
+*/
 
+public class Application {
+	String appName;
+	int appSize;
+	
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	public String getAppName(){
+		return this.appName;
+	}
+	public int getAppSize(){
+		return this.appSize;
+	}
+	public String toString(){
+		return "Application bane: "+this.appName+", with size: "+this.appSize;
+	}
+}
