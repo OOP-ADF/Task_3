ce682b9e1492fc2371d21ff7acc875c444ac8e96
widyawirachantika <wirachantika@gmail.com>
2016-02-03 18:50:20 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,45 @@
+public class Member{
+	private String name;
+	private String specialization;
+	private int projectWorked = 0;
+	
+	public void Member(String name){
+		this.name = name;
+	}
+	
+	public void Member(String name, String specialization){
+		this.name = name;
+		this.specialization = specialization;
+	}
+	
+	public void setPrivateName(String name){
+		this.name = name;
+	}
+		
+	public String getPrivateName(){
+		return name;
+	}
+	
+	public void setPrivateSpecialization(String specialization){
+		this.specialization = specialization;
+	}
+		
+	public String getPrivateString(){
+		return specialization;
+	}
+	
+	public void setPrivateProjectWorked(int projectWorked){
+		this.projectWorked = projectWorked;
+		projectWorked++;
+	}
+		
+	public int getPrivateProjectWorked(){
+		return projectWorked;
+	}
+	
+	public String toString(){
+		return ("Member"+name+"specialized in "+specialization+ " and already worked on "+projectWorked +" projects");
+	}
+	
+}
 
