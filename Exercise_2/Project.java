7eebaa57412005107cc86fd2d2104fd3f229a68c
FIRDA AMINY MA'RUF <firda_maruf@yahoo.com>
2016-02-03 15:11:44 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,36 @@
-
+public class Project {
+    private String projectName;
+    private Member[] teamMember=new Member[5];
+    private int nTeam=0;
+    private boolean releaseStatus=false;
+    
+    public Project(String projectName){
+        this.projectName=projectName;
+    }
+    
+    public void addMember(Member m){
+        teamMember[nTeam]=m;
+        nTeam++;
+        m.setProjectWorked(m.getProjectWorked()+1);
+    }
+    
+    public boolean isReleased(){
+        return releaseStatus;
+    }
+    
+    public void releaseApp(){
+        releaseStatus=true;
+    }
+    
+    @Override
+    public String toString(){
+        String status="";
+        if (releaseStatus==true){
+            status="Project "+projectName+" is complete with team member of "+nTeam;
+        }
+        else{
+            status="Project "+projectName+" is in progress with team member of "+nTeam;
+        }
+        return status;
+    }
+}
