74e81377364dc4d8486984b5bf3a1520a5a149b4
Annisa Rizki Pratiwi Wibowo <annisaarizki@yahoo.co.id>
2016-02-03 15:51:26 +0800

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,16 @@
-
+public class Driver {
+	public static void main (String[] args) {
+		AppStore aps = new AppStore();
+		aps.createNewApp("appl",100);
+		aps.createNewApp("app2",200);
+		aps.createNewApp("app3",300);
+		aps.createNewApp("app4",400);
+		System.out.println(aps.toString());
+		System.out.println(aps.getApp(1).toString());
+		
+		SmartPhone sm = new SmartPhone();
+		sm.setMemory(500);
+		sm.addApplication(aps,1);
+		System.out.println(sm.toString());
+	}
+}
