2a6298029022653b6e8faff6b1169801da552756
yulizar <erwinyulizar14@gmail.com>
2016-02-03 11:48:20 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,28 @@
 
+/*
+Name : Erwin Yulizar F
+NIM  : 1301144031
+Class: IF-38-01
+*/
+
+public class Application {
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	
+	public int getAppSize(){
+		return appSize;
+	}
+	
+	public String toString(){
+		return ("Application name : "+appName+" with size : "+appSize+" MB");
+	}
+}
