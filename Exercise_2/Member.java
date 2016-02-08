a3bd735025b737c77f71d0c861488a159a88c392
MaulidyaSurya <lidya370@gmail.com>
2016-02-08 16:07:41 +0700

    task 3

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,19 @@
-
+public class Member {
+	private String name;
+	private String specialization;
+	private int projectWorked=0;
+	
+	public Member(String name, String specialization) {
+		this.name = name;
+		this.specialization = specialization;
+	}
+	public String getName(){
+		return name;
+    }
+    public int getSpecialization(){
+		return specialization;
+    }
+	public String toString(){
+        return "Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" project";
+    }
+}
--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,26 @@
+public class Project {
+	private String projectName;
+	private Member[] teamMember = new Member[5];
+	private int nTeam = 0;
+	private Boolean releaseStatus = false;
 
+	public Project(String projectName){
+		this.projectName = projectName;
+	}
+	public void addMember(Member m){
+		nTeam++;
+		teamMember[nTeam] = m;
+	}
+	public void releaseApp() {
+		releaseStatus = true;
+	}
+	public String toString(){
+		String kalimat;
+		if (releaseStatus == False){
+			kalimat = "on progress";
+		} else {
+			kalimat = "complete";
+		}
+        return "Project "+projectName+" status is  "+kalimat+" with team member of"+nTeam;
+    }
+}
