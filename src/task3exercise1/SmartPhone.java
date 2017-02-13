1e92b28edf560522c0b899881cdbf345623bc72f
Hardiyan Ichsan Gunawan <hardiyan.gunawan@gmail.com>
2017-02-13 22:17:13 +0700

    SmartPhone.java tunggu konfirmasi dari yang lain
    Tinggal error & bug checking

--- src/task3exercise1/SmartPhone.java
+++ src/task3exercise1/SmartPhone.java
@@ -3,54 +3,85 @@ package task3exercise1;
 public class SmartPhone{
     // 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : Hardiyan Ichsan Gunawan 
+    // NAMA : 1301154138 
     // 
     //------------------- your code here-----------------------
-    
+    private Application[] appList;
+    private int totalApp;
+    private int memory;
     //---------------------------------------------------------
 
     
     // 
     // 2. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154138
+    // NAMA : Hardiyan Ichsan Gunawan 
     // 
     //------------------- your code here-----------------------
+    
+    public void setMemory(int memory){
+        this.memory = memory;
+    }
+    
+    public int getMemory(){
+        return memory;
+    }
+    
+    public Application getApp (int Id){
+        return appList[Id];
+    }
+    
+    public int getTotalApp(){
+        return totalApp;
+    }
     
     //---------------------------------------------------------
 
     
     // 
     // 3. method addApplication( appStore , appId ) here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154138 
+    // NAMA : Hardiyan Ichsan Gunawan 
     //
     // get an Application object from array appList of an appStore 
     // with array index = appId, an Application can be installed 
     // if Application size is less than remaining memory size
     // 
     //------------------- your code here-----------------------
-    
+    public void addApplication(AppStore appStore, int appId){
+        if (getAppSize() < getRemainingSize() && totalApp < 10){
+            System.out.println("Not Enough Memory");
+        } else {
+            appList[totalApp] = Application(appStore.getApp(appId));
+            totalApp++;
+        }
+    }
     //---------------------------------------------------------
     
         
     // 
     // 4. method method getRemainingSize() : integer here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154138 
+    // NAMA : Hardiyan Ichsan Gunawan
     //
     // returns the remaining memory size of the smart phone
     //
     //------------------- your code here-----------------------
-    
+    public int getRemainingSize(){
+        int totalAppSize = 0;
+        for (int i=0; i<appList.length; i++){
+            totalAppSize += appList[i].getAppSize();
+        }
+        return memory - totalAppSize;
+    }
     //---------------------------------------------------------
         
     
     // 
     // 5. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154138 
+    // NAMA : Hardiyan Ichsan Gunawan 
     // 
     // returns String that contains the smart phone total memory size, 
     // number of application installed, and remaining memory size 
@@ -60,8 +91,10 @@ public class SmartPhone{
     //
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
+        return "Memory size "+getMemory()+", "+getTotalApp()+" application installed, remaining memory size: "+getRemainingSize();
     
     //---------------------------------------------------------
     }
+
+    
 }
