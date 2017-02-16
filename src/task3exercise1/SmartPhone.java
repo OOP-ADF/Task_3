54a16c89b00d2d5b58dc4b720a4738d83341e3a1
nafasamuth <nafasa.muth@outlook.com>
2017-02-16 23:20:41 +0800

    coba

--- src/task3exercise1/SmartPhone.java
+++ src/task3exercise1/SmartPhone.java
@@ -7,7 +7,11 @@ public class SmartPhone{
     // NAMA : ........................., 
     // 
     //------------------- your code here-----------------------
-    
+    private int totalApp =0;
+    private int memory = 0;
+    private Application[] appList = new Application[10];
+    private int RemainingSize = 0;
+    private int App = 0;
     //---------------------------------------------------------
 
     
@@ -17,51 +21,62 @@ public class SmartPhone{
     // NAMA : ........................., 
     // 
     //------------------- your code here-----------------------
-    
-    //---------------------------------------------------------
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
 
+    public int getTotalApp() {
+        return totalApp;
+    }
+    public Application[] getAppList() {
+        return appList;
+    }
+
+    public int getRemainingSize() {
+        return RemainingSize;
+    }
+
+    public int getApp() {
+        return App;
+    }
     
+
+    //---------------------------------------------------------
     // 
     // 3. method addApplication( appStore , appId ) here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
-    // get an Application object from array appList of an appStore 
+    // get an Application object from array appList of an appStore
     // with array index = appId, an Application can be installed 
     // if Application size is less than remaining memory size
     // 
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-    
-        
     // 
     // 4. method method getRemainingSize() : integer here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
     // returns the remaining memory size of the smart phone
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-        
-    
     // 
     // 5. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
-    // 
-    // returns String that contains the smart phone total memory size, 
-    // number of application installed, and remaining memory size 
-    // 
+    // NIM : ..........................,
+    // NAMA : .........................,
+    //
+    // returns String that contains the smart phone total memory size,
+    // number of application installed, and remaining memory size
+    //
     // example : "memory size 100MB, 5 application installed, 
     //            remaining memory size: 60MB"
     //
     public String toString() {
-    //------------------- your code here-----------------------
+        //------------------- your code here-----------------------
         return null;
-    
-    //---------------------------------------------------------
+        
+        //---------------------------------------------------------
     }
 }
