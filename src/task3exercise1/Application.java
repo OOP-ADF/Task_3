df63f5df80475fe37d4d304af9ffd2c7290f2094
KukuhSanddi <kukuhsanddi12@gmail.com>
2017-02-16 20:44:33 +0700

    Edit Class Application and Driver

--- src/task3exercise1/Application.java
+++ src/task3exercise1/Application.java
@@ -3,43 +3,58 @@ package task3exercise1;
 public class Application {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154264
+    // NAMA : Kukuh Sanddi Razaq
     //
     //------------------- your code here-----------------------
-    
+    private String appName;
+    private int appSize;
     //---------------------------------------------------------
     
     // 2. Declare Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154264 
+    // NAMA : Kukuh Sanddi Razaq 
     //
     // Constructor initialize application name and size (MB)
     //
     //------------------- your code here-----------------------
+    public Application (String appName, int appSize){
+        
+    }
+
     
     //---------------------------------------------------------
     
     
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154264
+    // NAMA : Kukuh Sanddi Razaq
     //
     //------------------- your code here-----------------------
+    public String getAppName() {
+        return appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+    
     
     //---------------------------------------------------------
     
     
     // 4. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154264
+    // NAMA : Kukuh Sanddi Razaq
     //
     // returns String that contains application name and application size 
     // 
     // example : "Application name: App1, with size: 3MB"
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
+    String a;
+    a= "Application Name : "+appName+" Application Size : "+appSize;
+    return a;
     
     //---------------------------------------------------------
     }
