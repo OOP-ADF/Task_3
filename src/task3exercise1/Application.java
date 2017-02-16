e54ef4a4c3f83a86331f035942be4300d2873a07
DanielChristopher <danielthegamemail@gmail.com>
2017-02-16 20:27:26 +0700

    Revert "1301154502_Lailis Saada_IF3912"
    This reverts commit c26fe006e2938716caf47478dbd8794704c05fb6.

--- src/task3exercise1/Application.java
+++ src/task3exercise1/Application.java
@@ -1,64 +1,47 @@
 package task3exercise1;
 
-public class Application extends AppStore {
+public class Application {
 
     // 1. Declare your private attributes here:
-    // NIM : 1301154502, 
-    // NAMA : Lailis Sa'adah, 
+    // NIM : .........................., 
+    // NAMA : ........................., 
     //
     //------------------- your code here-----------------------
-    
-    String appName;
-    int appSize;
     
     //---------------------------------------------------------
     
     // 2. Declare Constructor here:
-    // NIM : 1301154502, 
-    // NAMA : Lailis Sa'adah, 
+    // NIM : .........................., 
+    // NAMA : ........................., 
     //
     // Constructor initialize application name and size (MB)
     //
     //------------------- your code here-----------------------
     
-    public Application(String appName, int appSize) {
-        this.appName = appName;
-        this.appSize = appSize;
-    }    
-    
     //---------------------------------------------------------
-     
-
+    
+    
     // 3. Declare your Setter and Getter method here:
-    // NIM : 1301154502, 
-    // NAMA : Lailis Sa'adah, 
+    // NIM : .........................., 
+    // NAMA : ........................., 
     //
     //------------------- your code here-----------------------
     
-    public String getAppName() {
-        return appName;
-    
-    }   
-    
-    public int getAppSize() {    
-        return appSize;
-    }
-    
     //---------------------------------------------------------
+    
+    
     // 4. Create method toString() : String here:
-    // NIM : 1301154502, 
-    // NAMA : Lailis Sa'adah, 
+    // NIM : .........................., 
+    // NAMA : ........................., 
     //
-    // returns String that contains application name and application size
+    // returns String that contains application name and application size 
     // 
     // example : "Application name: App1, with size: 3MB"
     public String toString() {
-        //------------------- your code here-----------------------
-        System.out.println("Application name: "+appName+" with size: "+appSize); 
-        
+    //------------------- your code here-----------------------
         return null;
-        
-        //---------------------------------------------------------
+    
+    //---------------------------------------------------------
     }
 
 }
