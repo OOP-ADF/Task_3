24259d6b3a4c059f55e27085f8ff52158cb98f63
faza <faza@faza-Satellite-L645>
2017-02-17 01:24:10 +0700

    ini yg project.java nya jeng. sama ditambahin nama di application.java nya

--- src/task3exercise2/Project.java
+++ src/task3exercise2/Project.java
@@ -1,11 +1,18 @@
 package task3exercise2;
 
+import java.lang.reflect.Member;
+
 
 public class Project {
+    private String projectName;
+    private Member teamMember[] = new Member[5];
+    private int nTeam = 0;
+    private boolean releaseStatus = false;
+    
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : Faza Ahda
+    // NAMA : 1301154292 
     //
     // Constructor initialize project name, 
     // instantiate array member with size of [5],
@@ -20,55 +27,70 @@ public class Project {
     // NAMA : ........................., 
     //
     //------------------- your code here-----------------------
+    public Project(String projectName) {    
+        this.projectName = projectName;
+    }
+
+    public void addMember(Member[] m) {
+        this.teamMember = m;
+        this.nTeam++;
+    }
+
+    public void releaseApp() {
+        this.releaseStatus = true;
+    }
+
+    public boolean isReleased(){
+        if(releaseStatus = true){
+            return true;
+        } else return false;
+    }
+
+    
+
+    
+
     
     //---------------------------------------------------------
-    
-    
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-    
-    
     // 4. Declare method addMember( m : Member ) here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
     // add an object Member m into array team member, then
     // increment integer nTeam+1
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-    
-    
     // 5. Declare method releaseApp() here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
     // set release status = true
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-    
-    
     // 6. Create method toString() : String here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     //
-    // returns String that contains Project name, number of team member, 
-    // and release status 
+    // returns String that contains Project name, number of team member,
+    // and release status
     // 
     // example : "Project Exodus, status not releaser, with team member of 4"
     //
     public String toString() {
-    //------------------- your code here-----------------------
-        return null;
-    
-    //---------------------------------------------------------
+        //------------------- your code here-----------------------
+        if (releaseStatus=true){
+            return "Project "+this.projectName+", status is in progress, with team member of"+this.nTeam;
+        } else return "Project "+this.projectName+", status is not in progress, with team member of"+this.nTeam;
+        
+        
+        //---------------------------------------------------------
     }
 }
