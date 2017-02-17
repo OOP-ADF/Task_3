efbaf720deb57eb5a5ae8cb9f911435e1e53dc8f
Irvan Nur Apendi <irvannurapendi@gmail.com>
2017-02-17 08:16:08 +0700

    Update driver

--- src/task3exercise2/Driver.java
+++ src/task3exercise2/Driver.java
@@ -6,10 +6,10 @@
 package task3exercise2;
 
 /**
- * NIM 1 : ................ 
- * NIM 2 : ................ 
- * NIM 3 : ................
- *
+ * NIM 1 : 1301150054
+ * NIM 2 : 1301154208 
+ * NIM 3 : 1301154502
+ * KELAS : IF-39-12
  */
 public class Driver {
 
@@ -24,11 +24,17 @@ public class Driver {
         // instantiate 5 new Member objects 
         // with name = Alex, Beni, Cakra, Deni, and Eric; 
         // and add them to StartUp stp
-        m = new Member("Alex");
-        myStartUp.addMember( m );
-        m = new Member("Beni");
-        myStartUp.addMember( m );
-        ...
+        Member m1 = new Member("Alex");
+        myStartUp.addMember( m1 );
+        Member m2 = new Member("Beni");
+        myStartUp.addMember( m2 );
+        Member m3 = new Member("Cakra");
+        myStartUp.addMember( m3 );
+        Member m4 = new Member("Deni");
+        myStartUp.addMember( m4 );
+        Member m5 = new Member("Eric");
+        myStartUp.addMember( m5 );
+        
         
         
         // create 2 Project object within the Start Up stp, 
@@ -38,20 +44,24 @@ public class Driver {
         
         // set project member as below
         // add Alex, Beni, and Deni into Project1        
-        p = myStartUp.getProject( 0 );
-        m = myStartUp.getMember( 0 );
+        p = myStartUp.getProject( 1 );
+        m1 = myStartUp.getMember( 1 );
         p.addMember(m); 
-        m = myStartUp.getMember( 1 );
-        p.addMember(m);
-        ...
+        p = myStartUp.getProject( 1 );
+        m2 = myStartUp.getMember( 2 );
+        p.addMember(m2);
+        
         
         // add Beni, Cakra, Deni, and Eric into Project2  
-        p = myStartUp.getProject( 1 );
-        m = myStartUp.getMember( 1 );
+        p = myStartUp.getProject( 2 );
+        m3 = myStartUp.getMember( 3 );
         p.addMember(m); 
-        m = myStartUp.getMember( 2 );
+        p = myStartUp.getProject( 2 );
+        m4 = myStartUp.getMember( 4 );
         p.addMember(m);
-        ...
+        p = myStartUp.getProject( 2 );
+        m5 = myStartUp.getMember( 5 );
+        
         
         // set release one of the project
         p = myStartUp.getProject( 1 );
@@ -60,15 +70,18 @@ public class Driver {
         // display the status of every member in Start Up myStartUp
         System.out.println( myStartUp.getMember( 0 ) );
         System.out.println( myStartUp.getMember( 1 ) );
-        ...
+        System.out.println( myStartUp.getMember( 2 ) );
+        System.out.println( myStartUp.getMember( 3 ) );
+        System.out.println( myStartUp.getMember( 4 ) );
+        System.out.println( myStartUp.getMember( 5 ) );
         
         // display the status of every project in Start Up myStartUp
-        System.out.println( myStartUp.getProject( 0 ) );
         System.out.println( myStartUp.getProject( 1 ) );
-        ...
+        System.out.println( myStartUp.getProject( 2 ) );
+        
         
         // display the number of released project in Start Up stp
-        System.out.println( ... );
+        System.out.println("Project yang sudah release adalah : " + myStartUp.getNumReleaseProject());
         
         // Try another scenario case on your own
         
