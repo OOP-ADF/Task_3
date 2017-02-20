4151a17ffb06288583ddc0a22917d85c08c2b11c
ranestari <ranestari10@gmail.com>
2017-02-20 17:14:06 +0700

    Merge origin/master
    Conflicts:
    	src/task_2/Mahasiswa.java

--- src/task3exercise1/AppStore.java
+++ src/task3exercise1/AppStore.java
@@ -3,53 +3,54 @@ package task3exercise1;
 public class AppStore {
     
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM :1301154553 .........................., 
+    // NAMA : Ranestari Sastriani........................., 
     //
-    //------------------- your code here-----------------------
-    
-    //---------------------------------------------------------
+   private Application[100] appList;
+   private int totalApp;
+   
+ 
     
     // 2. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
-    //
-    //------------------- your code here-----------------------
+    // NIM : 1301154553.........................., 
+    // NAMA : Ranestari Sastriani........................., 
+    public void setTotalApp(int totalApp) {
+        this.totalApp = totalApp;
+    }
+
+    public int getTotalApp() {
+        return totalApp;
+    }
     
-    //---------------------------------------------------------
     
-    
+
     // 3. Create method createNewApp( appName, appSize ) here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154553..........................,
+    // NAMA :Ranestari Sastriani ........................., 
     // instantiate new Application object and insert it in array appList
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
     
-    
-    // 4. Create method getApp( id ) : Application here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+
+// 4. Create method getApp( id ) : Application here:
+    // NIM : 1301154553..........................,
+    // NAMA : Ranestari Sastriani........................., 
     // returns Application object from array appList with array index = id
     //
     //------------------- your code here-----------------------
-    
     //---------------------------------------------------------
-        
-    
     // 5. Create method toString() : String here:
-    // NIM : .........................., 
+    // NIM : ..........................,
     // NAMA : ........................., 
     // @return returns String that contains the number of Application available
     // 
     // example : "There are 5 applications ready to install"
     // 
     public String toString() {
-    //------------------- your code here-----------------------
+        //------------------- your code here-----------------------
         return null;
-    
-    //---------------------------------------------------------
+        
+        //---------------------------------------------------------
     }
 }
