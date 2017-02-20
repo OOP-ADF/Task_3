9931c1700e971c47b9be3be59ddae32c38ee5529
shabranmsm <shabranmetafarady@gmail.com>
2017-02-20 19:50:04 +0700

    1301154413_Shabran

--- src/task3exercise2/Member.java
+++ src/task3exercise2/Member.java
@@ -3,34 +3,66 @@ package task3exercise2;
 public class Member {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154413 
+    // NAMA : MUHAMMAD SHABRAN METAFARADY 
     //
     //------------------- your code here-----------------------
-    
+        private String name;
+        private String specialization;
+        private int projectWorked = 0;
     //---------------------------------------------------------
     
     // 2. Declare your 2 Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154413 
+    // NAMA : MUHAMMAD SHABRAN METAFARADY
     //
     //------------------- your code here-----------------------
-    
+        public Member(String name) {
+            this.name = name;
+        }
+        
+        
+        public Member(String name, String specialization) {
+            this.name = name;
+            this.specialization = specialization;
+        }
     //---------------------------------------------------------
-    
-    
+        
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154413 
+    // NAMA : MUHAMMAD SHABRAN METAFARADY
     //
     //------------------- your code here-----------------------
+        public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+    
+    
+    public int getProjectWorked() {
+        return projectWorked;
+    }
+
+    public void setProjectWorked(int projectWorked) {
+        this.projectWorked = projectWorked;
+    }
     //---------------------------------------------------------
-    
     
     // 4. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154413 
+    // NAMA : MUHAMMAD SHABRAN METAFARADY
     //
     // returns String that contains member name, specialization, 
     // and number of project worked  
@@ -39,8 +71,7 @@ public class Member {
     //            worked on 5 project(s)"
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
-    
+        return "Member " + name + " specialized in " + specialization + " and already worked on " + projectWorked + " project(s)";
     //---------------------------------------------------------
     }
 
