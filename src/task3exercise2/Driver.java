f4711d1c8454b5940180c0c440f2fd476adba3cd
ranestari <ranestari10@gmail.com>
2017-02-27 21:54:11 +0700

    dah

--- src/task3exercise2/Driver.java
+++ src/task3exercise2/Driver.java
@@ -6,9 +6,9 @@
 package task3exercise2;
 
 /**
- * NIM 1 : ................ 
- * NIM 2 : ................ 
- * NIM 3 : ................
+ * NIM 1 : 1301154553 
+ * NIM 2 : 1301154567
+ * NIM 3 : 1301154469
  *
  */
 public class Driver {
@@ -28,6 +28,12 @@ public class Driver {
         myStartUp.addMember( m );
         m = new Member("Beni");
         myStartUp.addMember( m );
+        m = new Member("cakra");
+        myStartUp.addMember( m );
+        m = new Member("deni");
+        myStartUp.addMember( m );
+        m = new Member("eric");
+        myStartUp.addMember( m );
         ...
         
         
@@ -43,6 +49,8 @@ public class Driver {
         p.addMember(m); 
         m = myStartUp.getMember( 1 );
         p.addMember(m);
+        m = myStartUp.getMember( 2 );
+        p.addMember(m);
         ...
         
         // add Beni, Cakra, Deni, and Eric into Project2  
@@ -51,6 +59,11 @@ public class Driver {
         p.addMember(m); 
         m = myStartUp.getMember( 2 );
         p.addMember(m);
+        m = myStartUp.getMember( 3 );
+        p.addMember(m);
+        m = myStartUp.getMember( 4 );
+        p.addMember(m);
+        ...
         ...
         
         // set release one of the project
@@ -60,15 +73,17 @@ public class Driver {
         // display the status of every member in Start Up myStartUp
         System.out.println( myStartUp.getMember( 0 ) );
         System.out.println( myStartUp.getMember( 1 ) );
-        ...
-        
+        System.out.println( myStartUp.getMember( 2 ) );
+        System.out.println( myStartUp.getMember( 3 ) );
+        System.out.println( myStartUp.getMember( 4 ) );
+       
         // display the status of every project in Start Up myStartUp
         System.out.println( myStartUp.getProject( 0 ) );
         System.out.println( myStartUp.getProject( 1 ) );
         ...
         
         // display the number of released project in Start Up stp
-        System.out.println( ... );
+         System.out.println( myStartUp.getNumReleasedProject() );
         
         // Try another scenario case on your own
         
